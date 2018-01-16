d5010018b18dd8575435d247c0e917e5c6d5a755
John Skilbeck <john.skilbeck@gmail.com>
2018-01-16 00:43:33 -0800

    Pass all the options in the jdbc uri

--- aws-anarchy/src/aws_anarchy/sql.clj
+++ aws-anarchy/src/aws_anarchy/sql.clj
@@ -16,21 +16,11 @@
         (recur (string/replace sql (str k) (str (jdbc/sql-value v)))
                others)))))
 
-(defn get-athena-conn []
-  (DriverManager/getConnection
-   (-> :jdbc-athena-uri env util/decrypt)
-   (doto (Properties.)
-     (.put "user"           (-> :aws-access-key-id env util/decrypt))
-     (.put "password"       (-> :aws-secret-access-key env util/decrypt))
-     (.put "s3_staging_dir" (-> :s3-staging-dir env util/decrypt))
-     #_(.put "log_path"       "/tmp/athenalog.out")
-     #_(.put "log_level"      "DEBUG"))))
-
 (defn query-athena
   ([sql]
    (query-athena sql {}))
   ([sql params]
-   (with-open [conn (get-athena-conn)]
+   (with-open [conn (DriverManager/getConnection (-> :jdbc-athena-uri env util/decrypt))]
      (let [sql     (-> sql
                        (string/replace #";" "")
                        (string/replace #"--" "")
