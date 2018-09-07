092eb2a31d388c7ee584f6a151c611db766032a2
John Skilbeck <john.skilbeck@gmail.com>
2018-09-06 19:00:56 -0700

    wip

--- markets-etl/include/aws_lambda.clj
+++ markets-etl/include/aws_lambda.clj
@@ -1,5 +1,7 @@
 (ns jobs.aws-lambda
-  (:require [clojure.data.json :as json]
+  (:require [clj-time.core :as time]
+            [clj-time.format :as format]
+            [clojure.data.json :as json]
             [clojure.java.io :as io]
             [environ.core :refer [env]]
             [jobs.currency :as currency]
@@ -12,14 +14,41 @@
     :name jobs.aws-lambda
     :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]))
 
-(defn main []
+(def cli-options
+  [["-d" "--date DATE" "Start date (month) (yyyy-mm-dd format) to start processing"
+    :default  util/last-week]
+   ["-h" "--help"]])
+
+(defn -main [& args]
+  (equities/-main '-d '"2018-01-01")
+  (let [{:keys [options summary errors]} (cli/parse-opts args cli-options)
+        _              (println "Starting jobs... ")
+
+        _              (println "Currency... ")
+        currency       (currency/-main '-d (-> options :date))
+
+        _              (println "Economics... ")
+        economics      (economics/-main'-d (-> options :date))
+
+        _              (println "Equities... ")
+        equities       (equities/-main'-d (-> options :date))
+
+        _              (println "Interest rates... ")
+        interest-rates (interest-rates/-main'-d (-> options :date))
+
+        _              (println "Real estate... ")
+        real-estate    (real-estate/-main'-d (-> options :date))
+
+        _              (println "Finished!")]))
+
+(defn main [& args]
   (let [_              (println "Starting jobs... ")
 
         _              (println "Currency... ")
         currency       (currency/-main)
 
         _              (println "Economics... ")
-        econmics       (economics/-main)
+        economics      (economics/-main)
 
         _              (println "Equities... ")
         equities       (equities/-main)
