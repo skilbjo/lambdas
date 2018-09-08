a7a389d3aa326ebf6ab40460f5a3336c28ad70c9
John Skilbeck <john.skilbeck@gmail.com>
2018-09-07 18:42:49 -0700

    wip

--- markets-etl/include/aws_lambda.clj
+++ markets-etl/include/aws_lambda.clj
@@ -3,6 +3,8 @@
             [clj-time.format :as format]
             [clojure.data.json :as json]
             [clojure.java.io :as io]
+            [clojure.tools.cli :as cli]
+            [clojure.tools.logging :as log]
             [environ.core :refer [env]]
             [jobs.currency :as currency]
             [jobs.economics :as economics]
@@ -20,60 +22,46 @@
    ["-h" "--help"]])
 
 (defn -main [& args]
-  (let [{:keys [options summary errors]} (cli/parse-opts args cli-options)
-        _              (println "Starting jobs... ")
+  (let [{:keys [options summary errors]} (cli/parse-opts args cli-options)]
+    (log/info "Starting jobs... ")
+    ;(log/info "Currency... ")
+    ;(currency/-main '-d (-> options :date))
 
-        _              (println "Currency... ")
-        currency       (currency/-main '-d (-> options :date))
+    ;(log/info "Economics... ")
+    ;(economics/-main '-d (-> options :date))
 
-        _              (println "Economics... ")
-        economics      (economics/-main'-d (-> options :date))
+    ;(log/info "Equities... ")
+    ;(equities/-main '-d (-> options :date))
 
-        _              (println "Equities... ")
-        equities       (equities/-main'-d (-> options :date))
+    ;(log/info "Interest rates... ")
+    ;(interest-rates/-main '-d (-> options :date))
 
-        _              (println "Interest rates... ")
-        interest-rates (interest-rates/-main'-d (-> options :date))
+    ;(log/info "Real estate... ")
+    ;(real-estate/-main '-d (-> options :date))
 
-        _              (println "Real estate... ")
-        real-estate    (real-estate/-main'-d (-> options :date))
-
-        _              (println "Finished!")]))
-
-        ;_              (println "Starting jobs... ")
-        ;_              (println "Currency... ")
-        ;currency       (currency/-main '-d (-> options :date))
-        ;_              (println "Economics... ")
-        ;economics      (economics/-main'-d (-> options :date))
-        ;_              (println "Interest rates... ")
-        ;interest-rates (interest-rates/-main'-d (-> options :date))
-        ;_              (println "Real estate... ")
-        ;real-estate    (real-estate/-main'-d (-> options :date))
+    (log/info "Equities... ")
+    ;(equities/-main "-d" "2018-01-02")
+    (equities/-main "-d" (-> options :date))
+    (log/info "Finished!")))
 
 (defn main [& args]
-  (let [_              (println "Starting jobs... ")
-
-        _              (println "Currency... ")
+  (let [_              (log/info "Starting jobs... ")
+        _              (log/info "Currency... ")
         currency       (currency/-main)
-
-        _              (println "Economics... ")
+        _              (log/info "Economics... ")
         economics      (economics/-main)
-
-        _              (println "Equities... ")
+        _              (log/info "Equities... ")
         equities       (equities/-main)
-
-        _              (println "Interest rates... ")
+        _              (log/info "Interest rates... ")
         interest-rates (interest-rates/-main)
-
-        _              (println "Real estate... ")
+        _              (log/info "Real estate... ")
         real-estate    (real-estate/-main)
+        _              (log/info "Finished!")]
 
-        _              (println "Notifying healthchecks.io ... ")
-        _              (util/notify-healthchecks-io (-> :healthchecks-io-api-key
-                                                        env
-                                                        util/decrypt))
-
-        _              (println "Finished!")]))
+    (log/info "Notifying healthchecks.io ... ")
+    (util/notify-healthchecks-io (-> :healthchecks-io-api-key
+                                     env
+                                     util/decrypt))))
 
 (defn -handleRequest [_ event _ context]
   (let [event' (-> event
